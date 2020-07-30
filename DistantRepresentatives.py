d2cdb250f316a8ae772d8f327f39ecab135e1a06
Graeme Stroud <graeme.stroud@mail.utoronto.ca>
2020-07-30 17:53:29 -0400

    Added comments and descriptions to the Distant Representatives functions.

--- DistantRepresentatives.py
+++ DistantRepresentatives.py
@@ -1,9 +1,11 @@
 import math
 from Matching import BipartiteGraph
 
+def LInf(a,b):
+    return max(abs(a[0]-b[0]), abs(a[1]-b[1]))
 
 class Disc(object):
-    """docstring for Disc."""
+    """A Disc object."""
 
     def __init__(self, cx, cy, R):
         super(Disc, self).__init__()
@@ -24,18 +26,32 @@ class Disc(object):
         return ( self.cy - self.R  )
 
 
-#TODO!!!!!***************************
-#
 def contains_point(d, delta):
     """
-    For the given disc d, returns whether grid delta*LAMBDA intersects d.
+    For the given disc d, returns whether grid delta*LAMBDA intersects d
+    (as defined in 'Cabello's Approximation Algorithms for Spreading Points',
+    LAMBDA is a unit grid, and delta*LAMBDA is the grid with point spread out
+    delta apart, i.e. { delta*p : p in LAMBDA }). Can think of p as 'moving
+    through time' as we increase/decrease delta.
 
     Returns:
-    (0,None) - if contains a point.
-    (1,p, Q) - if hits a grid edge, and p is such a point of intersection
-    (2, p, Q) - if within cell, p = d[0,1] = center of disc
+    (0,None) - If d contains a grid point. Such point is not needed now, so None
+                is returned along with indicator 0.
+    (1,p, Q) - If d hits a grid edge. p is a point that intersects d and the
+                grid edge. Q containts the two grid points that make this grid
+                edge.
+    (2, p, Q) - Othwerwise, meaning d is contained within a grid cell.
+                p is the center of disc, and Q consists of the 4 corners of the
+                grid cell.
 
-    TODO Q are grid points, so make them not delta * point, but just point
+    TODO:
+    For Q above, since they are grid points, they return the points in grid
+    LAMBDA, as opposed to delta*LAMBDA, which is the relevant grid (this ensures
+    the points are integer points, saving the need to convert to the
+    delta*LAMBDA grid until later. This helps avoid any arithmetic errors).
+
+    We do not this for p, because it is part of the disc, and not a grid point.
+    Discs are fixed size, grids are not.
     """
 
     # Outside the square/disc, or touching the edge
@@ -113,11 +129,14 @@ def contains_point(d, delta):
 
 
 def inDisc(p, d):
+    """Returns true iff p is in disc d"""
     x, y = p
     cx, cy, R = d
     return cx - R  <= x and x <= cx + R and cy- R  <= y and y <= cy + R
 
 def getGridPointsInDiscStupid(d, delta, Q):
+    """In a brute force fashion, finds all grid points within the disc,
+    excluding elements of Q."""
     cx, cy, R = d
     xmin = math.floor((cx-R)/delta)
     xmax = math.ceil((cx+R)/delta)
@@ -136,16 +155,37 @@ def getGridPointsInDiscStupid(d, delta, Q):
 
 
 def getGridPointsInDisc():
-    # See Cabello for how to do this efficently
+    # See Cabello's paper for how to do this efficently
+    """In a smart way, find all grid points within the disc."""
     raise NotImplementedError
 
 def Placement(D, delta):
+    """
+    Given a set of L_infinity discs D=[d1,d2,d3,...,dn], and real value delta.
+
+    Returns a placement of representatives p=(p1,p2,...,pn), which satisfies:
+    - d(pi,pj) >= delta    for all i != j
+
+    This will always succeed when delta <= delta*/2, where delta* is the largest
+    value for which you can find a placement.
+
+    This may succeed when delta*/2 < delta <= delta*, but this is not
+    guaranteed. This will fail when delta > delta*, as no such placement exists.
+    """
+
     Q = set()
     p = [None] * len(D)
 
     # Booleans telling us is square are "small" or not.
     contains_point_bools = [contains_point(d, delta) for d in D]
 
+    # These discs contain no grid points, so their representatives are:
+    # - the centre, if the disc is completely contained within a cell.
+    # - a point on a grid edge, if the disc touches a grid edge.
+    # Q collects all grid points that are within delta distance of the Placement
+    #   of these representatives.
+    # The centres/edge points may be too close to each other, this will be
+    # checked later.
     for i,d in enumerate(D):
 
         if contains_point_bools[i][0] != 0:
@@ -154,7 +194,7 @@ def Placement(D, delta):
             if contains_point_bools[i][0] == 1: # Touches edge
                 p[i] = contains_point_bools[i][1]
                 Q = Q.union(set(contains_point_bools[i][2]))
-                #TODO vertices of E to Q
+
             else: # Is a small rectangle,
                 p[i] = (cx, cy)
 
@@ -162,13 +202,16 @@ def Placement(D, delta):
 
     P = set()
     Pi = [set()]*len(D)
+
+    # The remaining discs:
+    # Find all the grid points within each disc
     for i,d in enumerate(D):
         if contains_point_bools[i][0] == 0:
             Pi[i] = getGridPointsInDiscStupid(d, delta, Q)
             P = P.union(Pi[i])
 
+    # Enumerate all the grid points.
     P = list(P)
-
     P_index = dict()
     for i,x in enumerate(P):
         P_index[x] = i
@@ -178,13 +221,17 @@ def Placement(D, delta):
 
     n = len(D)
     m = len(P)
-    G = BipartiteGraph(n, m)
 
+    # Create a bipartite graph between all the discs (L) and all the grid points (R)
+    # within the discs. Add an edge between a disc and a grid point iff the grid
+    # point is in that disc.
+    G = BipartiteGraph(n, m)
     for i,d in enumerate(D):
         for x in Pi[i]:
             G.addEdge(i, P_index[x])
 
-
+    # Find a maximal matching that tries to ensure everything on the left side
+    #  is matched.
     M = G.matching()
     if M is None:
         print("FAIL: no matching")
@@ -192,10 +239,12 @@ def Placement(D, delta):
 
     numMatchingEdges = sum([sum(x) for x in M])
 
+    # Assert all discs get matched to a grid point.
     if numMatchingEdges < n - num_small_discs:
         print("FAIL: no matching")
         return None
 
+    # Convert grid points to corresponding point in delta sized grid.
     for i in range(n):
         for j in range(i+1, m):
             if M[i][j] == 1:
@@ -206,6 +255,7 @@ def Placement(D, delta):
         print("Fail: no matching")
         return None
 
+    # Ensure all points are >= delta distance apart.
     for i in range(n):
         for j in range(n):
             if i !=j:
@@ -217,13 +267,21 @@ def Placement(D, delta):
     return p
 
 def DistantRepresentatives(D):
+    """
+    Given a set of discs D, tries to find a placement of representatives points
+    in each disc so that all points are as far apart as possible.
 
+    This algorithm will succeed at finding points that are >= 0.5 delta* apart,
+    where delta* is the distance between the closest pair of points in the
+    optimal solution. In other words, this returns a 2-Approximation.
+    """
 
     assert(len(D) >=  1)
     if len(D) == 1:
         pass
 
-
+    # The optimal solutions takes on a special form. Generates all deltas
+    # that the optimal could be. Takes O(n^3) space/time.
     deltas = set()
     n = len(D)
     for i in range(n):
@@ -265,6 +323,10 @@ def DistantRepresentatives(D):
 
     print("L = ", L)
 
+    # Perform a binary Search
+    # Find delta_i where Placement(delta_i /2) succeeds but
+    #  Placement(delta_{i+1} /2) fails. The solution for delta_i/2 is a
+    #   2-Approximation.
     p_best = None
     while i >= l and i <= u and not (u - l <= 1):
 
